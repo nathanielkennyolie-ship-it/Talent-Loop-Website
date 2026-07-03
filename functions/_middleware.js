fcdd92855da2e7845c78370357ad603e3bc6e1e7
Nathaniel Kenny Olie <nathanielkennyolie@gmail.com>
2026-07-03 19:51:32 +0100

    fix: restructure site with directories + index.html for native clean URL support on Cloudflare Pages

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,22 +1,3 @@
 export async function onRequest(context) {
-  const { request, next } = context;
-  const url = new URL(request.url);
-  const path = url.pathname;
-
-  const pageMap = [
-    '/about', '/jobs', '/blog', '/assessment',
-    '/contact', '/testimonials', '/legal', '/privacy',
-  ];
-
-  if (pageMap.includes(path)) {
-    url.pathname = path + '.html';
-    return next(new Request(url.toString(), request));
-  }
-
-  if (path.startsWith('/blog/') && !path.endsWith('.html')) {
-    url.pathname = path + '.html';
-    return next(new Request(url.toString(), request));
-  }
-
-  return next();
+  return context.next();
 }
